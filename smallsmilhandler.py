fd7d907b46e2e2d4d9d60f61d175c440fe5bee06
cristpm <crisdario21@gmail.com>
2016-10-08 23:14:08 +0200

    cambio en la lista para guardar los datos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,12 +4,13 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
     """
     Clase para manejar smil
     """
 
-    def __init__ (self):
+    def __init__(self):
         """
         Constructor. Inicializamos las variables
         """
@@ -19,27 +20,25 @@ class SmallSMILHandler(ContentHandler):
         """
         Método que se llama cuando se abre una etiqueta
         """
-        datos_etiqueta = {}
-        root = ['width','height','background-color']
-        reg = ['id','top','bottom','left','right']
-        img = ['sre','region','begin','dur']
-        aud = ['sre','begin','sur']
-        text = ['sre','region']
-        etiquetas = {'root-layout':root,'region':reg,
-                        'img':img,'audio':aud,'textstream':text}
-                        
+        dat_etiqueta = {}
+        dat_atrib = {}
+        root = ['width', 'height', 'background-color']
+        reg = ['id', 'top', 'bottom', 'left', 'right']
+        img = ['sre', 'region', 'begin', 'dur']
+        aud = ['sre', 'begin', 'sur']
+        text = ['sre', 'region']
+        etiquetas = {'root-layout': root, 'region': reg, 'img': img,
+         'audio': aud, 'textstream': text}
         if name in etiquetas:
-            datos_etiqueta['etiqueta'] = name
             for atributo in etiquetas[name]:
-                if attrs.get(atributo,"") != "":
-                    datos_etiqueta[atributo] = attrs.get(atributo,"")
-            self.misdatos.append(datos_etiqueta)
-            ## datos_etiqueta.clear()
-            
-    def get_tags (self):
+                if attrs.get(atributo, "") != "":
+                    dat_atrib[atributo] = attrs.get(atributo, "")
+            dat_etiqueta[name] = dat_atrib
+            self.misdatos.append(dat_etiqueta)
+
+    def get_tags(self):
         return self.misdatos
-        
-            
+
 if __name__ == "__main__":
     """
     Programa principal
@@ -48,8 +47,8 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
-    misdatos= cHandler.get_tags()
-    for etiqueta in misdatos:
-        print('==========')
-        print(etiqueta)
-        print('==========')
+    misdatos = cHandler.get_tags()
+    for e in misdatos:
+        print("========================")
+        print(e)
+        print("========================")
