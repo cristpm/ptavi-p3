fe02aba40bfa957b6904de7714f639ac625b66a9
Cristian Pilco Miranda <cdpilco@alpha19.aulas.gsyc.urjc.es>
2016-10-10 13:47:47 +0200

    implementado la clase KaraokeLocal

--- karaoke.py
+++ karaoke.py
@@ -8,49 +8,58 @@ import urllib
 import sys
 import json
 
-
-def __srt__(datos):
-    for tag in datos:
-        for etiqueta, atributos in tag.items():
-            linea = etiqueta + "\t"
-            for c, v in atributos.items():
-                linea = linea + c +"="+ "\""+ v + "\""+"\t" 
-            print(linea)
-            
-
-def to_json(name_fichero, datos):
-    f = name_fichero[:-4] + "json"
-    json.dump(datos,open(f,'w'))
+try:
+    fichero = sys.argv[1]
+except IndexError:
+    sys.exit('Usage: python3 karaoke.py file.smil.')
     
+fich_json = fichero[:-4] + "json"
+
+class KaraokeLocal():
+
+    def __init__ (self, fichero):
+        parser = make_parser()
+        self.cHandler = SmallSMILHandler()
+        parser.setContentHandler(self.cHandler)
+        parser.parse(open(fichero))
+        self.misdatos = self.cHandler.get_tags()
         
-def do_local(datos):
-    for dic in datos:
-        for tag, atributos in dic.items():
-            if 'src' in atributos:
-                name = atributos['src']
-                if name[:4] == 'http':
-                    local = name.split('/')[-1]
-                    urllib.request.urlretrieve(name,local)
-                    atributos['src'] = local
-         
+    def __srt__(self):
+        for tag in self.misdatos:
+            for etiqueta, atributos in tag.items():
+                linea = etiqueta + "\t"
+                for c, v in atributos.items():
+                    linea = linea + c +"="+ "\""+ v + "\""+"\t" 
+                print(linea)
+                
+    def to_json(self, f, f_json = fich_json):
+        json.dump(self.misdatos,open(f_json,'w'))
+        
+    def do_local(self):
+        for dic in self.misdatos:
+            for tag, atributos in dic.items():
+                if 'src' in atributos:
+                    name = atributos['src']
+                    if name[:4] == 'http':
+                        local = name.split('/')[-1]
+                        urllib.request.urlretrieve(name,local)
+                        atributos['src'] = local
     
-
 
 if __name__ == "__main__":
     """
     Programa principal
     """
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
     try:
         fichero = sys.argv[1]
     except IndexError:
         sys.exit('Usage: python3 karaoke.py file.smil.')
-        
-    parser.parse(open(fichero))
-    misdatos = cHandler.get_tags()
-    __srt__(misdatos)
-    to_json(fichero, misdatos)
-    do_local(misdatos)
-    __srt__(misdatos)
+    
+    karaoke = KaraokeLocal(fichero)
+    karaoke.__srt__()
+    karaoke.to_json(fichero)
+    karaoke.do_local()
+    karaoke.to_json(fichero,'local.json')
+    print('=============================')
+    karaoke.__srt__()
+    
